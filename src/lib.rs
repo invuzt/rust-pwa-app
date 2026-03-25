0f63131d8d5624e2cbf5988257829ae8cb1a3c53
invuzt <invuzt@gmail.com>
2026-03-25 12:08:33 +0700

    Update: Rust nulis teks ke Home

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 use wasm_bindgen::prelude::*;
+use web_sys::window;
 
-// Kita "meminjam" fungsi window.alert dari JavaScript
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-// Fungsi ini yang akan dipanggil saat PWA dibuka
 #[wasm_bindgen(start)]
-pub fn run() {
-    alert("Halo! Ini adalah pesan dari Rust Engine 🦀");
+pub fn run() -> Result<(), JsValue> {
+    // Ambil object window browser
+    let window = window().expect("Gak ada window global");
+    let document = window.document().expect("Gak bisa ambil document");
+    
+    // Cari elemen h1 dengan id="status"
+    let val = document.get_element_by_id("status")
+        .expect("Elemen id='status' gak ketemu");
+    
+    // Ubah isi teksnya lewat Rust!
+    val.set_inner_html("Mantap! Teks ini dikirim langsung dari Rust Engine 🦀");
+
+    Ok(())
 }
 
